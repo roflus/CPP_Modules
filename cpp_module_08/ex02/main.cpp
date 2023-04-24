d782490180ba6b41cea77bf05da166f63fe3c083
roflus <95986204+roflus@users.noreply.github.com>
2023-04-24 22:36:48 +0200

    extra test

--- cpp_module_08/ex02/main.cpp
+++ cpp_module_08/ex02/main.cpp
@@ -1,13 +1,15 @@
 #include "MutantStack.hpp"
+#include <vector>
+#include <list>
 
 int main()
 {
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
-    std::cout << mstack.top() << std::endl;
+    std::cout << "Top: " << mstack.top() << std::endl;
     mstack.pop();
-    std::cout << mstack.size() << std::endl;
+    std::cout << "Size: " << mstack.size() << std::endl;
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
@@ -19,9 +21,34 @@ int main()
     --it;
     while (it != ite)
     {
+        std::cout << "Stack: ";
         std::cout << *it << std::endl;
         ++it;
     }
     std::stack<int> s(mstack);
+
+    std::cout << "---------List Test---------" << std::endl;
+    std::list<int> lstack;
+    lstack.push_back(5);
+    lstack.push_back(17);
+    std::cout << "Top: " << lstack.back() << std::endl;
+    lstack.pop_back();
+    std::cout << "Size: " << lstack.size() << std::endl;
+    lstack.push_back(3);
+    lstack.push_back(5);
+    lstack.push_back(737);
+    //[...]
+    lstack.push_back(0);
+    std::list<int>::iterator lit = lstack.begin();
+    std::list<int>::iterator lite = lstack.end();
+    ++lit;
+    --lit;
+    while (lit != lite)
+    {
+        std::cout << "List: ";
+        std::cout << *lit << std::endl;
+        ++lit;
+    }
+    std::list<int> l(lstack);
     return 0;
 }
